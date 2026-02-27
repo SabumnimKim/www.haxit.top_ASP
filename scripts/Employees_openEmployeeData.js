185777cc8983c07b0989f76e041e737c408caa24
Kim Gylling <kim.gylling@hotmail.com>
2026-02-27 18:55:34 +0200

    Add files via upload

---
+++ scripts/Employees_openEmployeeData.js
@@ -0,0 +1,11 @@
+function openEmployeeData(id)
+{
+
+	const xhttp = new XMLHttpRequest();
+	xhttp.onload = function() 
+	{
+		document.getElementById("employeesright").innerHTML = this.responseText;
+	}
+	xhttp.open("GET", "employees_right.asp?id="+id+"", true);
+	xhttp.send();
+}
